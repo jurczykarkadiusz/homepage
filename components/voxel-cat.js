ce85012929b70b1a76dd75ee9fd1048c5469bfae
jurczykarkadiusz <jurczykarkadiusz@gmail.com>
2023-09-15 09:18:17 +0200

    modified:   components/voxel-cat.js

--- components/voxel-cat.js
+++ components/voxel-cat.js
@@ -1,73 +1,73 @@
-import React, { useState, useEffect, useRef, useCallback } from 'react'
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { loadGLTFModel } from '../lib/model' // Import your custom model loader
-import { CatSpinner, CatContainer } from './voxel-cat-loader'
+import React, { useState, useEffect, useRef, useCallback } from 'react';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { loadGLTFModel } from '../lib/model'; // Import your custom model loader
+import { CatSpinner, CatContainer } from './voxel-cat-loader';
 
 function easeOutCirc(x) {
-  return Math.sqrt(1 - Math.pow(x - 1, 4))
+  return Math.sqrt(1 - Math.pow(x - 1, 4));
 }
 
 const VoxelCat = () => {
-  const refContainer = useRef()
-  const [loading, setLoading] = useState(true)
-  const refRenderer = useRef()
-  const urlCatGLB = (process.env.NODE_ENV === 'production' ? 'https://craftzdog.global.ssl.fastly.net/homepage' : '') + '/images/cat.glb'
+  const refContainer = useRef();
+  const [loading, setLoading] = useState(true);
+  const refRenderer = useRef();
+  const localFilePath = '/images/cat.glb'; // Assuming cat.glb is in your project directory
 
   const handleWindowResize = useCallback(() => {
-    const { current: renderer } = refRenderer
-    const { current: container } = refContainer
+    const { current: renderer } = refRenderer;
+    const { current: container } = refContainer;
     if (container && renderer) {
-      const scW = container.clientWidth
-      const scH = container.clientHeight
-      renderer.setSize(scW, scH)
+      const scW = container.clientWidth;
+      const scH = container.clientHeight;
+      renderer.setSize(scW, scH);
     }
-  }, [])
+  }, []);
 
   /* eslint-disable react-hooks/exhaustive-deps */
   useEffect(() => {
-    const { current: container } = refContainer
+    const { current: container } = refContainer;
     if (container) {
-      const scW = container.clientWidth
-      const scH = container.clientHeight
+      const scW = container.clientWidth;
+      const scH = container.clientHeight;
 
       const renderer = new THREE.WebGLRenderer({
         antialias: true,
-        alpha: true
+        alpha: true,
       });
-      renderer.setPixelRatio(window.devicePixelRatio)
-      renderer.setSize(scW, scH)
-      renderer.outputEncoding = THREE.sRGBEncoding
-      container.appendChild(renderer.domElement)
-      refRenderer.current = renderer
+      renderer.setPixelRatio(window.devicePixelRatio);
+      renderer.setSize(scW, scH);
+      renderer.outputEncoding = THREE.sRGBEncoding;
+      container.appendChild(renderer.domElement);
+      refRenderer.current = renderer;
 
       // Create a scene and add lighting
-      const scene = new THREE.Scene()
+      const scene = new THREE.Scene();
 
       // Add a hemisphere light for ambient illumination
-      const ambientLight = new THREE.HemisphereLight(0xffffff, 0x080820, 3)
-      scene.add(ambientLight)
+      const ambientLight = new THREE.HemisphereLight(0xffffff, 0x080820, 3);
+      scene.add(ambientLight);
 
       // Create a directional light for casting shadows
-      const directionalLight = new THREE.DirectionalLight(0xffffff, 5)
-      directionalLight.position.set(20, 10, 20)
-      directionalLight.castShadow = true
-      scene.add(directionalLight)
+      const directionalLight = new THREE.DirectionalLight(0xffffff, 5);
+      directionalLight.position.set(20, 10, 20);
+      directionalLight.castShadow = true;
+      scene.add(directionalLight);
 
       // Set up shadow properties for the directional light
-      directionalLight.shadow.mapSize.width = 1024
-      directionalLight.shadow.mapSize.height = 1024
-      directionalLight.shadow.camera.near = 0.5
-      directionalLight.shadow.camera.far = 500
+      directionalLight.shadow.mapSize.width = 1024;
+      directionalLight.shadow.mapSize.height = 1024;
+      directionalLight.shadow.camera.near = 0.5;
+      directionalLight.shadow.camera.far = 500;
 
-      const target = new THREE.Vector3(-0.5, 1.2, 0)
+      const target = new THREE.Vector3(-0.5, 1.2, 0);
       const initialCameraPosition = new THREE.Vector3(
         30 * Math.sin(0.2 * Math.PI),
         10,
         20 * Math.cos(0.2 * Math.PI)
       );
 
-      const scale = scH * 0.005 + 4.8
+      const scale = scH * 0.005 + 4.8;
       const camera = new THREE.OrthographicCamera(
         -scale,
         scale,
@@ -75,64 +75,60 @@ const VoxelCat = () => {
         -scale,
         0.01,
         10000
-      )
-      camera.position.copy(initialCameraPosition)
-      camera.lookAt(target)
+      );
+      camera.position.copy(initialCameraPosition);
+      camera.lookAt(target);
 
-      const controls = new OrbitControls(camera, renderer.domElement)
-      controls.autoRotate = true
-      controls.target = target
+      const controls = new OrbitControls(camera, renderer.domElement);
+      controls.autoRotate = true;
+      controls.target = target;
 
-      loadGLTFModel(scene, urlCatGLB, {
+      loadGLTFModel(scene, localFilePath, {
         receiveShadow: true,
-        castShadow: true
+        castShadow: true,
       }).then(() => {
-        animate()
-        setLoading(false)
-      })
+        animate();
+        setLoading(false);
+      });
 
-      let req = null
-      let frame = 0
+      let req = null;
+      let frame = 0;
       const animate = () => {
-        req = requestAnimationFrame(animate)
+        req = requestAnimationFrame(animate);
 
-        frame = frame <= 100 ? frame + 1 : frame
+        frame = frame <= 100 ? frame + 1 : frame;
 
         if (frame <= 100) {
-          const p = initialCameraPosition
-          const rotSpeed = -easeOutCirc(frame / 120) * Math.PI * 20
+          const p = initialCameraPosition;
+          const rotSpeed = -easeOutCirc(frame / 120) * Math.PI * 20;
 
-          camera.position.y = 10
-          camera.position.x =
-            p.x * Math.cos(rotSpeed) + p.z * Math.sin(rotSpeed)
-          camera.position.z =
-            p.z * Math.cos(rotSpeed) - p.x * Math.sin(rotSpeed)
-          camera.lookAt(target)
+          camera.position.y = 10;
+          camera.position.x = p.x * Math.cos(rotSpeed) + p.z * Math.sin(rotSpeed);
+          camera.position.z = p.z * Math.cos(rotSpeed) - p.x * Math.sin(rotSpeed);
+          camera.lookAt(target);
         } else {
-          controls.update()
+          controls.update();
         }
 
-        renderer.render(scene, camera)
-      }
+        renderer.render(scene, camera);
+      };
 
       return () => {
-        cancelAnimationFrame(req)
-        renderer.domElement.remove()
-        renderer.dispose()
-      }
+        cancelAnimationFrame(req);
+        renderer.domElement.remove();
+        renderer.dispose();
+      };
     }
-  }, [])
+  }, []);
 
   useEffect(() => {
-    window.addEventListener('resize', handleWindowResize, false)
+    window.addEventListener('resize', handleWindowResize, false);
     return () => {
-      window.removeEventListener('resize', handleWindowResize, false)
-    }
-  }, [handleWindowResize])
+      window.removeEventListener('resize', handleWindowResize, false);
+    };
+  }, [handleWindowResize]);
 
-  return (
-    <CatContainer ref={refContainer}>{loading && <CatSpinner />}</CatContainer>
-  )
-}
+  return <CatContainer ref={refContainer}>{loading && <CatSpinner />}</CatContainer>;
+};
 
-export default VoxelCat
+export default VoxelCat;
