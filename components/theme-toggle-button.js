bc46580090b07d3fbc9df68663638f34ee4dd993
jurczykarkadiusz <jurczykarkadiusz@gmail.com>
2023-09-16 11:41:49 +0200

    modified:   components/theme-toggle-button.js

--- components/theme-toggle-button.js
+++ components/theme-toggle-button.js
@@ -17,7 +17,7 @@ const ThemeToggleButton = () => {
       >
         <IconButton
           aria-label="Toggle theme"
-          colorScheme={useColorModeValue('purple', 'orange')}
+          colorScheme={useColorModeValue('blackAlpha.600', 'orange')}
           icon={useColorModeValue(<MoonIcon />, <SunIcon />)}
           onClick={toggleColorMode}
         ></IconButton>
