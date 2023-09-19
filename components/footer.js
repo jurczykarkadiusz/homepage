255968898cbf111124e880dc6a4bfa84de245770
Arkadiusz <46725496+jurczykarkadiusz@users.noreply.github.com>
2023-09-19 08:39:14 +0200

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ import { Box } from '@chakra-ui/react'
 const Footer = () => {
   return (
     <Box align="center" opacity={0.4} fontSize="sm" position="relativee">
-      &copy; {new Date().getFullYear()} Arkadiusz Jurczyk. All Rights Reserved.
+      &copy;2019 - {new Date().getFullYear()} Arkadiusz Jurczyk. All Rights Reserved.
     </Box>
   )
 }
